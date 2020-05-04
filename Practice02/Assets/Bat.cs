8052e17cf9273f70309c554c3d944e44767de526
Lemonder791123 <63327445+Lemonder791123@users.noreply.github.com>
2020-05-04 21:39:40 +0800

    練習2

---
+++ Practice02/Assets/Bat.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Bat : MonoBehaviour
+{
+    public int hp; //= 250;
+    public int atk = 50;
+    public int heal = 10;
+    public Text system;
+    public Slime slime;
+
+    public void Atk()
+    {
+        slime.Hurt();
+        //return; //slime.hp;
+    }
+
+    public int Hurt()
+    {
+        int HP = hp - slime.atk;
+        hp = HP;
+        system.text = "蝙蝠 - 受到傷害：" + slime.atk + "\n" + "蝙蝠 - 血量剩下：" + hp;
+        return hp;
+    }
+
+    public void Heal()
+    {
+        int HP = (hp + heal);
+        hp = HP;
+        system.text = "蝙蝠 - 受到治癒：" + heal + "\n" + "蝙蝠 - 血量剩下：" + hp;
+        return;
+    }
+}
---
+++ Practice02/Assets/Slime.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Slime : MonoBehaviour
+{
+    public int hp; //= 250;
+    public int atk = 10;
+    public int heal = 50;
+    public Text system;
+    public Bat bat;
+
+    public void Atk()
+    {
+        bat.Hurt();
+        //return; //bat.hp;
+    }
+
+    public int Hurt()
+    {
+        int HP = hp - bat.atk;
+        hp = HP;
+        system.text = "史萊姆 - 受到傷害：" + bat.atk + "\n" + "史萊姆 - 血量剩下：" + hp;
+        return hp;
+    }
+
+    public void Heal()
+    {
+        int HP = (hp + heal);
+        hp = HP;
+        system.text = "史萊姆 - 受到治癒：" + heal + "\n" + "史萊姆 - 血量剩下：" + hp;
+        return;
+    }
+}
